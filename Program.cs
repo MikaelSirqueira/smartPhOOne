b61493c9c3aaacd79bd8a87bedea90a9f634c0f2
Mikael Sirqueira <silmike14@yahoo.com.br>
2023-12-26 17:01:46 -0300

    :tada: feat: implementado as funcionalidades faltantes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "99223344", modelo: "Tijolão", imei: "111144", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Teste Smartphone Iphone");
+Iphone iphone = new Iphone(numero: "88776655", modelo: "15 Pro Max", imei: "00003", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
